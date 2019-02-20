6dc2618ffaf9076b6818b1d0a67a6bac39acc545
Zach Oriel <Zacharyaaron15@gmail.com>
2019-02-19 17:32:38 -0800

    Ohhh
    ohh

--- Assets/Scripts/Menu/SelectionManager.cs
+++ Assets/Scripts/Menu/SelectionManager.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class SelectionManager : MonoBehaviour
 {
-    public GameObject characterCardPrefab;
+    public GameObject characterCardPrefab, panelParent;
 
 	// Use this for initialization
 	void Awake ()
     {
-		
+		//for (int characterCards = 0; characterCards < controller.numberOfPlayers; characterCards++)
+  //      {
+  //          GameObject newCharCard = Instantiate(characterCardPrefab, panelParent.transform);
+  //      }
 	}
 }
