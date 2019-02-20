5b4fe2e47499aa25f46e4701b7e871178f6da2e2
Zach Oriel <Zacharyaaron15@gmail.com>
2019-02-20 01:23:43 -0800

    Got 4 player character selection fully implemented

--- Assets/Scripts/Menu/SelectionManager.cs
+++ Assets/Scripts/Menu/SelectionManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SelectionManager : MonoBehaviour
 {
     public GameObject characterCardPrefab, panelParent;
+    public GameObject[] characterCards;
 
 	// Use this for initialization
 	void Awake ()
@@ -13,5 +14,33 @@ public class SelectionManager : MonoBehaviour
         {
             GameObject newCharCard = Instantiate(characterCardPrefab, panelParent.transform);
         }
+
+        characterCards = GameObject.FindGameObjectsWithTag("CharacterCard");
+    }
+
+    /// yeah this is gonna need some seeeerious re-working after prototype, fam
+    void Update()
+    {
+        #region Don't Look At Me I'm Hideous
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            characterCards[0].GetComponent<CharacterCardGenerator>().GenerateCard();
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            characterCards[1].GetComponent<CharacterCardGenerator>().GenerateCard();
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            characterCards[2].GetComponent<CharacterCardGenerator>().GenerateCard();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            characterCards[3].GetComponent<CharacterCardGenerator>().GenerateCard();
+        }
+        #endregion
     }
 }
