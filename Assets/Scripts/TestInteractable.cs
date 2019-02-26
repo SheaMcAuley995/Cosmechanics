3f3856d1b3d1894e58eec385668e89c09296b93f
Shea.McAuley <thespookter5123@gmail.com>
2019-02-25 17:52:08 -0800

    WIP of new Interact scripts

--- Assets/Scripts/TestInteractable.cs
+++ Assets/Scripts/TestInteractable.cs
@@ -2,13 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestInteractable : MonoBehaviour, IInteractable {
+public class TestInteractable : MonoBehaviour, IInteractable, IPickUpable {
 
-    public delegate void myCurrentInteractions();
+    public Transform travelToTransform;
+    public float travelToSpeed;
+    private Rigidbody rb;
     //public myCurrentInteractions
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     public void InteractWith()
     {
         Debug.Log("Calling interface");
     }
+    public void PickUp()
+    {
+
+    }
+    public void DropObject()
+    {
+
+    }
+
+    IEnumerator UpdatePickup()
+    {
+        while(true)
+        {
+            rb.velocity = (travelToTransform.position - transform.position).normalized;
+            yield return null;
+        }
+    }
+
 }
