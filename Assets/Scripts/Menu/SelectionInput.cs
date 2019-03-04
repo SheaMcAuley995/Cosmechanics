04ffead31d6a3bb855fb1ba3b0dae9f36dd1ab7c
Zach Oriel <Zacharyaaron15@gmail.com>
2019-03-04 11:25:11 -0800

    I think I've fixed the selection bug?

--- Assets/Scripts/Menu/SelectionInput.cs
+++ Assets/Scripts/Menu/SelectionInput.cs
@@ -5,18 +5,22 @@ using Rewired;
 
 public class SelectionInput : MonoBehaviour
 {
-    public int playerId = 0;
-    Player players;
+ //   public int playerId = 0;
+ //   Player players;
+ //   bool input;
 
-	// Use this for initialization
-	void Awake ()
-    {
-        players = ReInput.players.GetPlayer(playerId);
-    }
+	//// Use this for initialization
+	//void Awake ()
+ //   {
+ //       players = ReInput.players.GetPlayer(playerId);
+ //   }
 	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
+	//// Update is called once per frame
+	//void Update ()
+ //   {
+	//	if (players.GetButtonDown("Interact"))
+ //       {
+ //           GetComponent<CharacterCardGenerator>().GenerateCard();
+ //       }
+	//}
 }
