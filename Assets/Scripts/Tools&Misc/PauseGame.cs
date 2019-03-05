79f028b26f327814c8de3355177d17a0eb51e1de
Zach Oriel <Zacharyaaron15@gmail.com>
2019-03-04 17:46:00 -0800

    Working on UI stuff

---
+++ Assets/Scripts/Tools&Misc/PauseGame.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseGame : MonoBehaviour
+{
+    [Header("UI Elements")]
+    public Canvas pauseCanvas;
+    public Image pausePanel;
+
+    [Header("Configurations")]
+    public KeyCode pauseButton1;
+    public KeyCode pauseButton2;
+
+    Color menuFaded = new Color(0f, 0.5108867f, 1f, 0f);
+    Color menuTransparent = new Color(0f, 0.5108867f, 1f, 0.5882353f);
+    bool input;
+
+    #region KeyCode caching
+    KeyCode defaultPauseButton1 = KeyCode.Escape;
+    KeyCode defaultPauseButton2 = KeyCode.P;
+    KeyCode none = KeyCode.None;
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        SetDefaultButtons();
+	}
+
+    void SetDefaultButtons()
+    {
+        if (pauseButton1 == none)
+        {
+            pauseButton1 = defaultPauseButton1;
+        }
+        if (pauseButton2 == none)
+        {
+            pauseButton2 = defaultPauseButton2;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        input = Input.GetKeyDown(pauseButton1) || Input.GetKeyDown(pauseButton2);
+
+        if (input)
+        {
+            pausePanel.color = Color.Lerp(menuFaded, menuTransparent, 2f);
+        }
+	}
+}
