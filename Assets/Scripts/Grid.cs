c3476a59ac161be459d33797baa55775fde5308d
Shea.McAuley <thespookter5123@gmail.com>
2019-03-06 12:54:50 -0800

    Grid system started

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    [SerializeField] Vector3 gizmosCubeSize;
+    [SerializeField] Vector3 gridSize;
+
+    List<Node> nodes;
+
+
+    private void OnDrawGizmosSelected()
+    {
+        for(int i = 0; i < gridSize.z; i++)
+        {
+            for(int j = 0; j < gridSize.x; j++)
+            {
+                //Gizmos.DrawWireCube(new Vector3(i,0,j) + transform.position, (gizmosCubeSize));
+                Node newNode = new Node();
+                newNode.transform.position = new Vector3(i, 0, j) + transform.position;
+                nodes.Add(newNode);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour  {
+
+    public bool walkable;
+    public Vector3 worldPosition;
+    public int gridX;
+    public int gridY;
+
+    public bool isFlamable;
+
+    public Node(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY)
+    {
+        walkable = _walkable;
+        worldPosition = _worldPos;
+        gridX = _gridX;
+        gridY = _gridY;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(0.8f,0.8f,0.8f));
+    }
+}
