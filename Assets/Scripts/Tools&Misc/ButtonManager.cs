dbf18fc9bd71abeba5be9f3a7275b57436c3f334
Zach Oriel <Zacharyaaron15@gmail.com>
2019-03-12 10:17:08 -0700

    Placeholder Damage UI & Death Screen

--- Assets/Scripts/Tools&Misc/ButtonManager.cs
+++ Assets/Scripts/Tools&Misc/ButtonManager.cs
@@ -19,4 +19,17 @@ public class ButtonManager : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void ReturnToMenu(string scene)
+    {
+        // TODO: Scene fader
+        SceneManager.LoadScene(scene);
+    }
+
+    public void RetryLevel(string thisScene)
+    {
+        // TODO: Scene fader 
+        // (also this might be tricker than I'm thinking cause we'll need to store character data?)
+        SceneManager.LoadScene(thisScene);
+    }
 }
