e069d47c2d8e0810ede221ab24bb98cce41332e2
m <mkaplor@gmail.com>
2019-03-25 12:24:43 -0700

    Added temporary florp shader

---
+++ Assets/Scripts/FlorpDespenser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlorpDespenser : MonoBehaviour , IInteractable {
+
+    public GameObject florpPrefab;
+    public static FlorpDespenser instance;
+    public Transform florpEjection;
+
+ 
+    void Start ()
+    {
+		
+	}
+
+    public void InteractWith()
+    {
+        Instantiate(florpPrefab, florpEjection);
+    }
+
+
+}
