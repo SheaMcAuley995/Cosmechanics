35376bd1e0e6b25d7fd46dae6fe83705820af162
m <mkaplor@gmail.com>
2019-04-01 10:06:24 -0700

    florp reviecer

--- Assets/FlorpReceptor.cs
+++ Assets/FlorpReceptor.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FlorpReceptor : MonoBehaviour, IInteractable {
+
+    public float maxFlorpAmount;
+    public float currentFlorp;
+
+    private void Start()
+    {
+        maxFlorpAmount = 200f;
+    }
     public void InteractWith()
     {
         if (true /*holding florp?*/)
         {
+            
             //remove florp from player
             //add amount florp to engines
         }
+        Debug.Log("current florp = " + currentFlorp);
     }
 
-    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Florp")
+        {
+            currentFlorp += other.GetComponent<Florp>().containedFlorp;
+            Destroy(other.gameObject);
+        }
+    }
 }
