29f85869aa5351ff3258c77fa8fcedabbd3e3efc
Zach Oriel <Zacharyaaron15@gmail.com>
2019-04-09 08:51:41 -0700

    Multiple back end things

--- Assets/Scripts/OverWorld/LevelSelectManager.cs
+++ Assets/Scripts/OverWorld/LevelSelectManager.cs
@@ -5,22 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelectManager : MonoBehaviour
 {
-	public void LaunchLevel(string scene)
+    public PlayerController[] players;
+
+    private void Awake()
+    {
+        players = FindObjectsOfType<PlayerController>();
+    }
+
+    public void LaunchLevel(string scene)
     {
         if (OverworldManager.instance.level == OverworldManager.Level.Level1)
         {
-            scene = "CopyOfMainMAp";
-            SceneManager.LoadScene(scene);
+            scene = "NewMichaelTest";
         }
         else if (OverworldManager.instance.level == OverworldManager.Level.Level2)
         {
             scene = "NewMichaelTest";
-            SceneManager.LoadScene(scene);
         }
         else if (OverworldManager.instance.level == OverworldManager.Level.Level3)
         {
-            scene = "ZachNewMichaelTest";
-            SceneManager.LoadScene(scene);
+            scene = "NewMichaelTest";
         }
+
+        SceneManager.LoadScene(scene);
     }
 }
