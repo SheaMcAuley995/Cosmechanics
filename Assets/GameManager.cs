77e46ac3b6e36f1cd1825c7c726643a92d50b8e4
Shea McAuley <thespookter5123@gmail.com>
2019-04-18 12:15:50 -0700

    Fixes to sceneselection

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -24,14 +24,14 @@ public class GameManager : MonoBehaviour {
         // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
         if (GUI.Button(new Rect(20, 40, 80, 20), "Level 1"))
         {
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(0);
 
         }
 
         // Make the second button.
         if (GUI.Button(new Rect(20, 70, 80, 20), "Level 2"))
         {
-
+            SceneManager.LoadScene(1);
         }
     }
 }
