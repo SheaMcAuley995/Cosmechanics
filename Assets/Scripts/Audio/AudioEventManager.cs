ff692bb888b12d0240a568b6bc22ba525408aa05
m <mkaplor@gmail.com>
2019-04-30 12:31:15 -0700

    added in audio... again

--- Assets/Scripts/Audio/AudioEventManager.cs
+++ Assets/Scripts/Audio/AudioEventManager.cs
@@ -71,7 +71,7 @@ public class AudioEventManager : MonoBehaviour {
     private void Start()
     {
         PlaySound("Theme");
-        PlaySound("Ambient", .8f);        
+        PlaySound("Ambient", .8f,1,0);        
     }
     
 
