8904b8d24b0410510601147629374c3e9bb14651
Zach Oriel <Zacharyaaron15@gmail.com>
2019-05-02 22:56:53 -0700

    Fixed important loop related bugs

--- Assets/Scripts/Menus/ButtonManager.cs
+++ Assets/Scripts/Menus/ButtonManager.cs
@@ -6,12 +6,14 @@ using UnityEngine.SceneManagement;
 public class ButtonManager : MonoBehaviour
 {
     // BUILD INDEX KEY:
-    // 0 = MainMenu
+    // 0 = MainMenu_Updated
     // 1 = CharacterSelection
     // 2 = ZachOverWorld
-    // 3 = NewMichaelTest
+    // 3 = BetaMichaelTest
+    // 4 = Ship_Level_1Final
 
     SelectedPlayer[] players;
+
 
     // Fades to character selection
     public void StartGame()
@@ -22,7 +24,6 @@ public class ButtonManager : MonoBehaviour
     // Fades to quit
     public void QuitGame()
     {
-
         SceneFader.instance.FadeToQuit();
     }
 
@@ -44,13 +45,28 @@ public class ButtonManager : MonoBehaviour
     // Fades to current scene
     public void RetryLevel()
     {
+        players = FindObjectsOfType<SelectedPlayer>();
+
         SceneFader.instance.FadeTo(SceneManager.GetActiveScene().name.ToString());
         Time.timeScale = 1f;
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i].transform.position = ExampleGameController.instance.spawnPoints[i];
+        }
     }
 
     // Fades to character selection
     public void ContinueGame()
     {
-        SceneFader.instance.FadeTo("CharacterSelection");
+        players = FindObjectsOfType<SelectedPlayer>();
+
+        SceneFader.instance.FadeTo("ZachOverWorld");
+        Time.timeScale = 1f;
+
+        foreach (SelectedPlayer player in players)
+        {
+            player.transform.localScale = new Vector3(0f, 0f, 0f);
+        }
     }
 }
