41e8d811dfd071a315459ecaf7b0d464cddffa99
Zach Oriel <Zacharyaaron15@gmail.com>
2019-05-07 12:58:44 -0700

    Made big changes to the crimes & sentences, getting lunch

--- Assets/Scripts/Menus/CharacterSelection/CharacterCardGenerator.cs
+++ Assets/Scripts/Menus/CharacterSelection/CharacterCardGenerator.cs
@@ -94,16 +94,17 @@ public class CharacterCardGenerator : MonoBehaviour
         newCharacter = new CharacterData(displayFields.nameField, displayFields.crimeField, displayFields.sentenceField);
 
         #region Generated List Content
+
+        // 4
         #region Video Feeds
-        /// 4 video feeds added to the list using the ListExtension class
         for (int i = 0; i < videoFeeds.Length; i++)
         {
             videoFeedList.AddMany(videoFeeds[i]);
         }
         #endregion
 
+        // 91
         #region Names
-        /// 91 names added to the list using the ListExtension class
         namesList.AddMany("Wroelk", "Gaohq", "Eisk", "Brozzyhm", "Struets",
             "Kruazek", "Strouerq", "Couz'lp", "Pheoblepsua", "Effyls",
             "Klecl", "Globbens", "Marohne", "Poesoth", "Treoffmm", "Knoziet",
@@ -122,76 +123,71 @@ public class CharacterCardGenerator : MonoBehaviour
             "Mith'ob Omega Supreme", "Corwaldron", "Zoriel", "EchoZoolu", "MimsyWinters");
         #endregion
 
+        // 52
         #region Crimes
-        /// 60 crimes added to the list using the ListExtension class
-        crimesList.AddMany("Astrolarsony", "Space-whale poaching",
-            "Not completing a dyson sphere", "Non-consensual evacuation of planets",
-            "Space littering", "Opening an airlock in the wrong situation",
-            "Serial cereal eating", "Accelerating the big crunch", "Public dabbing", 
-            "Overpaying crewmembers", "Wearing the wrong shoe size", "Lollygagging",
-            "Selling florponade without a permit", "Owning too many cats",
-            "Owning too few cats", "Sneezing loudly", "Leaving the water running",
-            "Spilling the florp", "Failure to properly shuffle cards", "Drawing on the table",
-            "Jigglegaffing within 30 parsecs of the big bang", "Incorrectly using the word 'blockchain'", 
-            "Unsanctioned rapid unscheduled disassembly", "Unlicensed terraforming", "Unsafe hyperlane change", 
-            "Calling yourself a space pirate", "Drinking the florp", "Panicking at the disco",
-            "Couldn't handle the truth", "Failing to study history... and then repeating it",
-            "Failing to provide more cowbell", "Failing to construct additional pylons",
-            "Fighting for their right to party", "Star arson", "Crashing the LAN party",
-            "Removing the watermark", "Coloring outside the lines", "Sneezing without covering their mouth",
-            "Leaving someone on 'read'", "Eating all of the cookies", "Novel reading",
-            "Failing a Turing test while biological",
-            "Feebleness of intellect", "Video game addiction", "Using only 10% of the brain",
-            "Use of imperial units instead of metric",
-            "Noise ordinance violation: playing music too loudly in space", "Poor taste in art",
-            "Untidy computer desktop", "Timeline destabilization",
-            "Desynchronizing the phase cycles of a quantum entanglement tunneler",
-            "Unsanctioned alteration of a moon's orbit", 
-            "Hitting all of the buttons on a space elevator",
-            "Speeding - going warp 80 in a warp 65 zone", "Public indecency and indecent publicity",
-            "Traumatizing a Class 0 Civilization with a fake alien invasion",
-            "Seeing someone sneeze... and not saying 'bless you'", "Florp-juggling without a license");
+        crimesList.AddMany("Astrolarsony", "Space littering", "Serial cereal eating", 
+            "Accelerating the big crunch", "Public dabbing", "Overpaying crewmembers", 
+            "Lollygagging", "Owning too many cats", "Owning too few cats", "Sneezing loudly", 
+            "Leaving the water running", "Spilling the florp", "Drawing on the table",             
+            "Unlicensed terraforming", "Unsafe hyperlane change", "Drinking the florp", 
+            "Panicking at the disco", "Couldn't handle the truth", "Star arson", 
+            "Crashing the LAN party", "Removing the watermark", "Coloring outside the lines",
+            "Leaving someone on 'read'", "Eating all of the cookies", "Novel reading",            
+            "Feebleness of intellect", "Video game addiction", "Poor taste in art", 
+            "Untidy computer desktop", "Timeline destabilization", "Illicit space-whale watching", 
+            "Misplacing warheads", "Unauthorized cat hoarding", "Inappropriate florp handling",
+            "Destroyed sacred meteor", "Piloting without a license", "Didn't return library book",
+            "Failed to return pen", "Ran a red light", "Scream-sneezed", "Unsanctioned docking",
+            "Speeding in the hangar", "Asked for the manager", "Brought baby to the movies",
+            "Forgot valentine's day", "Forgot their anniversary", "Tattled", "Pantsed a co-worker",
+            "Pirated movies", "Used water on an oil fire", "Put tin foil in the microwave",
+            "Ate soup with a fork");
         #endregion
 
+        // 14
         #region Sentences
-        /// 29 sentences added to the list using the ListExtension class
-        sentencesList.AddMany("1e+08 quantum galactic cycles cleaning the Aether",
-            "500 years in the spice mines of Druffel", "Food for the broodmother",
-            "Community service", "Rehabilitation classes", "Extra math homework",
-            "Fined 25 million spacecoin", "Infinite existence in a cyclical time loop",
-            "Doesn't have to go home, but they can't stay here",
-            "Handcuffed to their clone",
-            "To be exposed to a vacuum for approximately 3.4111111176 seconds",
-            "THE CLAW", "Something really, really bad", "Has to wear wet socks",
-            "No wifi", "Has to do the Druffel run in less than 11 parsecs",
-            "Must memorize the SpaceTunes terms and conditions",
-            "Internet access restricted to 56k modem",
-            "Has to program an operating system entirely in binary",
-            "10 years of hard labor as a trash compactor inspector",
-            "To be marooned on a desert planet with two suns and two troublesome droids",
-            "To be used to plug a hole the size of a dinner plate in the airlock",
-            "Must run a barefoot marathon on a path strewn with legos",                               
-            "Atmospheric reentry without a heat shield",
-            "To be replaced by a clone and forced to watch it live out it's life",
-            "To have 50% of all good memories removed",
-            "Banished to a wretched hive of scum and villainy",
-            "To be given laxatives and pushed into space so that their last act will be to boldly go where no one has gone before",
-            "To be stranded on an ocean planet near the event horizon of a black hole");
+        sentencesList.AddMany("Community service", "Rehabilitation classes", 
+            "Extra math homework", "Fined 25 million spacecoin", "Handcuffed to their clone",
+            "THE CLAW", "Has to wear wet socks", "No wifi", "Non-powered steering", 
+            "Tax rate doubled", "Doubled commute time", "100 push ups", "50 zero-g pull-ups",
+            "Music taken away");
         #endregion
 
-        #region Questionable Crimes & Sentences
-        questionables.AddMany("Public inflorpication", "Boring when inflorpicated", "Piloting under the influence of spice",
-            "Possession of florp with intent to redistribute", "Minus 50 DKP", "To be used as a test subject", "Involuntary organ donation",
-            "Disassembly by nanobots", "Brain to be removed from body and inserted into a manufacturing robot", 
-            "Body to be donated to science prior to death");
-        #endregion
-
+        // 4
         #region Materials
         for (int l = 0; l < materials.Length; l++)
         {
             materialList.AddMany(materials[l]);
         }
         #endregion
+
+        //#region Questionable Or Too Long Crimes & Sentences
+        //questionables.AddMany("Public inflorpication", "Boring when inflorpicated", "Piloting under the influence of spice",
+        //    "Possession of florp with intent to redistribute", "Minus 50 DKP", "To be used as a test subject", "Involuntary organ donation",
+        //    "Disassembly by nanobots", "Brain to be removed from body and inserted into a manufacturing robot", 
+        //    "Body to be donated to science prior to death", "1e+08 quantum galactic cycles cleaning the Aether",
+        //    "500 years in the spice mines of Druffel", "Infinite existence in a cyclical time loop",
+        //    "Doesn't have to go home, but they can't stay here", "To be exposed to a vacuum for approximately 3.4111111176 seconds",
+        //    "Has to do the Druffel run in less than 11 parsecs", "Must memorize the SpaceTunes terms and conditions",
+        //    "Internet access restricted to 56k modem", "Has to program an operating system entirely in binary",
+        //    "10 years of hard labor as a trash compactor inspector", "To be marooned on a desert planet with two suns and two troublesome droids",
+        //    "To be used to plug a hole the size of a dinner plate in the airlock", "Must run a barefoot marathon on a path strewn with legos",
+        //    "Atmospheric reentry without a heat shield", "To be replaced by a clone and forced to watch it live out it's life",
+        //    "To have 50% of all good memories removed", "Banished to a wretched hive of scum and villainy",
+        //    "To be given laxatives and pushed into space so that their last act will be to boldly go where no one has gone before",
+        //    "To be stranded on an ocean planet near the event horizon of a black hole", "Seeing someone sneeze... and not saying 'bless you'",
+        //    "Traumatizing a Class 0 Civilization with a fake alien invasion", "Public indecency and indecent publicity",
+        //    "Speeding - going warp 80 in a warp 65 zone", "Florp-juggling without a license", "Hitting all of the buttons on a space elevator",
+        //    "Unsanctioned alteration of a moon's orbit", "Desynchronizing the phase cycles of a quantum entanglement tunneler",
+        //    "Noise ordinance violation: playing music too loudly in space", "Use of imperial units instead of metric", "Using only 10% of the brain",
+        //    "Failing a Turing test while biological", "Sneezing without covering their mouth", "Fighting for their right to party",
+        //    "Failing to construct additional pylons", "Failing to provide more cowbell", "Failing to study history... and then repeating it",
+        //    "Calling yourself a space pirate", "Unsanctioned rapid unscheduled disassembly", "Incorrectly using the word 'blockchain'",
+        //    "Jigglegaffing within 30 parsecs of the big bang", "Failure to properly shuffle cards", "Selling florponade without a permit",
+        //    "Wearing the wrong shoe size", "Opening an airlock in the wrong situation", "Not completing a dyson sphere", "Space-whale poaching",
+        //    "Non-consensual evacuation of planets", "Something really, really bad", "Food for the broodmother");
+        //#endregion
+
         #endregion
 
         cameraMultiTarget = Camera.main.GetComponent<CameraMultiTarget>();
