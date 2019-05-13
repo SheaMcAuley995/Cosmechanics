5fd90e5ab0c2ce447787ba347a58ff013924c315
Zach Oriel <Zacharyaaron15@gmail.com>
2019-05-13 07:05:24 -0700

    Fixed some broken stuff but now florp receptor is broken too ughh

---
+++ Assets/Scripts/Tools&Misc/ForcingStuffLastMinute.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ForcingStuffLastMinute : MonoBehaviour
+{
+    PlayerController[] players;
+    Scene scene;
+
+	// AHHHHHHHHHHHHHHHHHHHHHHHH
+	void Start ()
+    {
+        players = FindObjectsOfType<PlayerController>();
+        scene = SceneManager.GetActiveScene();
+
+        if (scene.name == "Ship_Level_Tutorial NEW")
+        {
+            ExampleGameController.instance.transform.position = new Vector3(-1f, 0.05f, -4f);
+            StartCoroutine(AHHH());
+        }
+        else
+        {
+            ExampleGameController.instance.transform.position = new Vector3(-1.81f, 0.05f, -13.5f);
+        }
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i].gameObject.transform.position = ExampleGameController.instance.spawnPoints[i];
+        }
+	}
+
+    IEnumerator AHHH()
+    {
+        yield return new WaitForSeconds(0.2f);
+        for (int i = 0; i < players.Length; i++)
+        {
+            players[i].gameObject.transform.position = new Vector3(-1f, 0.05f, -4f);
+        }
+        yield return null;
+    }
+}
