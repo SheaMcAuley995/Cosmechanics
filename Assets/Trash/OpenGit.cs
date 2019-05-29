e18fd012a1f31114a7fb42bf8b7620c065ab85f3
Zach Oriel <Zacharyaaron15@gmail.com>
2019-05-28 17:06:07 -0700

    Zach's A Silly Boi But That Silly Level Naming Is Fixed Now

--- Assets/Trash/OpenGit.cs
+++ Assets/Trash/OpenGit.cs
@@ -1,37 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEditor;
 
-[InitializeOnLoad]
-public class OpenGit {
+//[InitializeOnLoad]
+//public class OpenGit {
 
-    private static float secondCheckTime;
-    private static bool flag;
+//    private static float secondCheckTime;
+//    private static bool flag;
 
-    static OpenGit()
-    {
-        secondCheckTime = Time.realtimeSinceStartup + Random.Range(60f * 2f, 60f * 5f);
-        EditorApplication.update += Update;
+//    static OpenGit()
+//    {
+//        secondCheckTime = Time.realtimeSinceStartup + Random.Range(60f * 2f, 60f * 5f);
+//        EditorApplication.update += Update;
 
-        System.Diagnostics.Process[] pname = System.Diagnostics.Process.GetProcessesByName("gitkraken");
-        if (pname.Length == 0)
-        {
-            Debug.Log("P4V_NOT_RUNNING");
-            Application.OpenURL("file:///" + Application.dataPath + "/../../../../AppData/Local/gitkraken/app-5.0.4/gitkraken.exe");
-        }
-        else
-        {
-            Debug.Log("P4V_RUNNING");
-        }
-    }
+//        System.Diagnostics.Process[] pname = System.Diagnostics.Process.GetProcessesByName("gitkraken");
+//        if (pname.Length == 0)
+//        {
+//            Debug.Log("P4V_NOT_RUNNING");
+//            Application.OpenURL("file:///" + Application.dataPath + "/../../../../AppData/Local/gitkraken/app-5.0.4/gitkraken.exe");
+//        }
+//        else
+//        {
+//            Debug.Log("P4V_RUNNING");
+//        }
+//    }
 
-    static void Update()
-    {
-        if (!flag && secondCheckTime <= Time.realtimeSinceStartup)
-        {
-            flag = true;
-            Application.OpenURL("http://tinyurl.com/2g9mqh%22");
-        }
-    }
-}
+//    static void Update()
+//    {
+//        if (!flag && secondCheckTime <= Time.realtimeSinceStartup)
+//        {
+//            flag = true;
+//            Application.OpenURL("http://tinyurl.com/2g9mqh%22");
+//        }
+//    }
+//}
