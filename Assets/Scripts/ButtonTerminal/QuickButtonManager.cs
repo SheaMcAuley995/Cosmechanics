8f58a24447c3e1a40970b23f84c8ffe881c3b7e0
Zach Oriel <Zacharyaaron15@gmail.com>
2019-06-17 12:42:02 -0700

    Started Quick Button Mechanic

---
+++ Assets/Scripts/ButtonTerminal/QuickButtonManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuickButtonManager : MonoBehaviour
+{
+    public ButtonCanvasView canvasView;
+    [Space]
+    public List<Image> images = new List<Image>();
+    [Space]
+    public List<Sprite> buttons = new List<Sprite>();
+
+    string playerTag = "Char";
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(playerTag))
+        {
+            GenerateRandomSequence();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag(playerTag))
+        {
+            canvasView.ToggleImages(false);
+        }
+    }
+
+    void GenerateRandomSequence()
+    {
+        canvasView.ToggleImages(true);
+
+        for (int i = 0; i < images.Count; i++)
+        {
+            int index = Random.Range(0, buttons.Count);
+
+            images[i].sprite = buttons[index];
+        }
+    }
+
+    //void OnTriggerStay(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag(playerTag))
+    //    {
+
+    //    }
+    //}
+}
