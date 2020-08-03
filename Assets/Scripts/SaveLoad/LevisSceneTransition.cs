dcdbed840f99eddf241675288064b38ea0e14d13
Levi <levirosenberg97@gmail.com>
2020-08-02 17:36:35 -0700

    Reworked the Level Select Scene to work with Rewired

---
+++ Assets/Scripts/SaveLoad/LevisSceneTransition.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using Rewired;
+
+public class LevisSceneTransition : MonoBehaviour
+{
+    public new LevisTransitionCamera camera;
+    TextMeshProUGUI buttonText;
+
+    Player player;
+
+    string sceneName;
+
+    private void Awake()
+    {
+        player = ReInput.players.GetPlayer(0);
+    }
+
+    private void Start()
+    {
+        player.AddInputEventDelegate(OnSelectPressed, UpdateLoopType.Update, InputActionEventType.ButtonJustPressed, "Select");
+
+        buttonText = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        sceneName = camera.target.name;
+        buttonText.text = "Load " + sceneName;
+    }
+
+    void OnSelectPressed(InputActionEventData data)
+    {
+        if(camera.worldSelected)
+        {
+            ChangeScene();
+        }
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+}
