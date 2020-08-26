30a17cb7f26159cc6cadc8c1dd0082925e2b7f06
Levi <levirosenberg97@gmail.com>
2020-08-25 17:25:16 -0700

    New Level Select Functional

---
+++ Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonClick : MonoBehaviour, ISelectHandler
+{
+    public AudioSource buttonClick;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        buttonClick.Play();
+    }
+}
