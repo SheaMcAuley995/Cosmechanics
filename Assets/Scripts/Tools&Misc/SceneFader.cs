857ca306aa3e91a5d65527fd024d38b8b4d2d753
Zach Oriel <Zacharyaaron15@gmail.com>
2020-12-15 21:55:34 -0800

    Attempted fix.

--- Assets/Scripts/Tools&Misc/SceneFader.cs
+++ Assets/Scripts/Tools&Misc/SceneFader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -96,7 +97,28 @@ public class SceneFader : MonoBehaviour
             yield return 0;
         }
 
-        SceneManager.LoadScene(scene);
+        SceneManager.LoadSceneAsync(scene);
+
+        try
+        {
+            Vector3[] spawnPoints;
+            spawnPoints = FindObjectOfType<SetSpawnPositions>().spawnpositions;
+
+            GameObject[] players;
+            players = CharacterHandler.instance.players;
+            for (int i = 0; i < CharacterHandler.instance.numberOfPlayers; i++)
+            {
+                players[i].transform.position = spawnPoints[i];
+                players[i].GetComponent<PlayerController>().enabled = true;
+                players[i].GetComponent<PlayerController>().cameraTrans = CameraMultiTarget.instance.GetComponent<Camera>().transform;
+
+            }
+            CameraMultiTarget.instance.SetTargets(CharacterHandler.instance.players);
+        }
+        catch(NullReferenceException e)
+        {
+            Debug.Log("This is fine.");
+        }
     }
 
     IEnumerator FadeOutToQuit()
