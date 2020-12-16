5d637a74c762420d736a93343f1e9240604a8cfe
Levi <levirosenberg97@gmail.com>
2020-12-15 19:06:42 -0800

    Add Pause Menu Controls to SetSpawnPositions

--- Assets/Scripts/SetSpawnPositions.cs
+++ Assets/Scripts/SetSpawnPositions.cs
@@ -1,15 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 public class SetSpawnPositions : MonoBehaviour
 {
-    //public GameObject pauseMenu;
+    PauseMenu pauseMenu;
     public GameObject inGameCanvas;
     public Vector3[] spawnpositions = new Vector3[4];
+    GameObject[] players;
+
     void Start()
     {
         CharacterHandler.instance.spawnPoints = spawnpositions;
+
+        players = CharacterHandler.instance.players;
+
+        foreach (GameObject player in players)
+        {
+            pauseMenu = FindObjectOfType<PauseMenu>();
+            if (pauseMenu != null)
+            {
+                player.GetComponent<PlayerController>().player.AddInputEventDelegate(pauseMenu.GetComponent<PauseMenu>().OnPauseUpdate, UpdateLoopType.Update, "Pause");
+            }
+            else
+            {
+                Debug.Log("Pause Menu is Missing");
+            }
+        }
     }
 
     private void OnDrawGizmos()
