1d9c026586e2a62d3fc509da1bfb97dfed103ad5
Levi <levirosenberg97@gmail.com>
2021-01-05 18:11:49 -0800

    Can now see frame rate alongside resolution

--- Assets/Scripts/AutomaticScroll.cs
+++ Assets/Scripts/AutomaticScroll.cs
@@ -6,7 +6,7 @@ using UnityEngine.EventSystems;
 
 public class AutomaticScroll : MonoBehaviour, IUpdateSelectedHandler
 {
-	private static float SCROLL_MARGIN = 0.3f; // how much to "overshoot" when scrolling, relative to the selected item's height
+	private static float SCROLL_MARGIN = 0.3f;
 
 	private ScrollRect sr;
 
@@ -17,39 +17,30 @@ public class AutomaticScroll : MonoBehaviour, IUpdateSelectedHandler
 
     public void OnUpdateSelected(BaseEventData eventData)
 	{
-		Debug.Log("hit");
-		// helper vars
 		float contentHeight = sr.content.rect.height;
 		float viewportHeight = sr.viewport.rect.height;
 
-		// what bounds must be visible?
-		float centerLine = eventData.selectedObject.transform.localPosition.y; // selected item's center
-		float upperBound = centerLine + (eventData.selectedObject.GetComponent<RectTransform>().rect.height / 2f); // selected item's upper bound
-		float lowerBound = centerLine - (eventData.selectedObject.GetComponent<RectTransform>().rect.height / 2f); // selected item's lower bound
+		float centerLine = eventData.selectedObject.transform.localPosition.y;
+		float upperBound = centerLine + (eventData.selectedObject.GetComponent<RectTransform>().rect.height / 2f);
+		float lowerBound = centerLine - (eventData.selectedObject.GetComponent<RectTransform>().rect.height / 2f);
 
-		// what are the bounds of the currently visible area?
 		float lowerVisible = (contentHeight - viewportHeight) * sr.normalizedPosition.y - contentHeight;
 		float upperVisible = lowerVisible + viewportHeight;
 
-		// is our item visible right now?
 		float desiredLowerBound;
 		if (upperBound > upperVisible)
 		{
-			// need to scroll up to upperBound
 			desiredLowerBound = upperBound - viewportHeight + eventData.selectedObject.GetComponent<RectTransform>().rect.height * SCROLL_MARGIN;
 		}
 		else if (lowerBound < lowerVisible)
-		{
-			// need to scroll down to lowerBound
+		{ 
 			desiredLowerBound = lowerBound - eventData.selectedObject.GetComponent<RectTransform>().rect.height * SCROLL_MARGIN;
 		}
 		else
 		{
-			// item already visible - all good
 			return;
 		}
 
-		// normalize and set the desired viewport
 		float normalizedDesired = (desiredLowerBound + contentHeight) / (contentHeight - viewportHeight);
 		sr.normalizedPosition = new Vector2(0f, Mathf.Clamp01(normalizedDesired));
 	}
