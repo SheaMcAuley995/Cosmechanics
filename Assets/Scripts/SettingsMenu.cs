0d5e791e11b1c43a7a7fa2b4ed1d6a205b6d1ba7
Levi <levirosenberg97@gmail.com>
2021-01-12 21:18:58 -0800

    Culled the resolution list to only show refresh rates of 30 or 60

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -15,7 +15,7 @@ public class SettingsMenu : MonoBehaviour {
 
     Resolution[] resolutions;
 
-    //List<Resolution> res;
+    List<Resolution> resList;
 
     public void Start()
     {
@@ -25,45 +25,31 @@ public class SettingsMenu : MonoBehaviour {
 
         List<string> options = new List<string>();
 
-        //res = new List<Resolution>();
+        resList = new List<Resolution>();
 
         int currentResolutionIndex = 0;
 
         for (int i = 0; i < resolutions.Length; i++)
         {
-            //bool dupe = false;
-            //for (int j = i+1; j < resolutions.Length; j++)
-            //{
-            //    if (resolutions[j].height == resolutions[i].height &&
-            //        resolutions[j].width == resolutions[i].height)
-            //    {
-            //        dupe = true;
-            //    }
-            //}
+            if (resolutions[i].refreshRate == 30 || resolutions[i].refreshRate == 60)
+            {
+                resList.Add(resolutions[i]);
+            }
 
-            //if (dupe == true)
-            //{
-            //    continue;
-            //}
-                //res.Add(resolutions[i]);
+        }
 
-                string option = resolutions[i].width + " x " + resolutions[i].height + " @" + resolutions[i].refreshRate + "hz";
+        for (int i = 0; i < resList.Count; i++)
+        {
+                string option = resList[i].width + " x " + resList[i].height + " @" + resList[i].refreshRate + "hz";
 
                 options.Add(option);
-            //else
-            //{
-                
-            //}
-
-                if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+                if (resList[i].width == Screen.currentResolution.width && resList[i].height == Screen.currentResolution.height)
                 {
                     currentResolutionIndex = i;
                 } 
+            
         }
 
-        //for (int i = 0; i < res.Count; i++)
-        //{
-        //}
 
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentResolutionIndex;
@@ -87,7 +73,7 @@ public class SettingsMenu : MonoBehaviour {
 
     public void SetResolution(int resolutionIndex)
     {
-        Resolution resolution = resolutions[resolutionIndex];
+        Resolution resolution = resList[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
